e7854e3208cecaaf5ab23339a9f9fc423ae105e7
Onely Garay <onelygaray2002@gmail.com>
2023-06-07 16:18:26 -0600

    Header and footer done

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,66 @@
+<footer class="w-full h-[362px] text-white flex items-center justify-center gap-[145px]">
+    <div class="flex w-[1410] h-[202px] gap-[145px] items-start">
+        <div class="w-[245px] h-[202px] flex items-start flex-col">
+            <div class="flex items-center justify-center gap-[10px] mb-[12px]">
+                <img src="{{asset('images/logo.svg')}}" alt="">
+                <h1 class="text-[36px] font-bold ">Axies</h1>
+            </div>
+            <div class="w-full h-[66px] text-[14px] leading-[22px]">
+                <p>Lorem ipsum dolor sit amet,consectetur
+                    adipisicing elit. Quis non, fugit totam vel
+                    laboriosam vitae.</p>
+            </div>
+            <div class="flex gap-[12px] items-center mt-[28px]">
+                <button class="w-[40px] h-[40px] bg-[#343444] rounded-[8px]"></button>
+                <button class="w-[40px] h-[40px] bg-[#343444] rounded-[8px]"></button>
+                <button class="w-[40px] h-[40px] bg-[#343444] rounded-[8px]"></button>
+                <button class="w-[40px] h-[40px] bg-[#343444] rounded-[8px]"></button>
+            </div>
+        </div>
+        <div class="flex gap-[140px] h-[186px] w-[565px]">
+
+            <div class="w-[108px] h-[186px] flex flex-col align-text-top ">
+                <h3 class="text-[18px] leading-[26px] font-[700]">My Account</h3>
+                <ul class="flex flex-col gap-[16px] w-full mt-[24px]">
+                    <li><a class="text-[14px] leading-[22px]">Authors</a></li>
+                    <li><a class="text-[14px] leading-[22px]">Collection</a></li>
+                    <li><a class="text-[14px] leading-[22px]">Author Profile</a></li>
+                    <li><a class="text-[14px] leading-[22px]">Create Collection</a></li>
+                </ul>
+            </div>
+            <div class="w-[96px] h-[186px] flex flex-col align-text-top ">
+                <h3 class="text-[18px] leading-[26px] font-[700]">Resources</h3>
+                <ul class="flex flex-col gap-[16px] w-[96px] mt-[24px]">
+                    <li><a class="text-[14px] leading-[22px]">Help & Support</a></li>
+                    <li><a class="text-[14px] leading-[22px]">Live Auctions</a></li>
+                    <li><a class="text-[14px] leading-[22px]">Item Details</a></li>
+                    <li><a class="text-[14px] leading-[22px]">Activity</a></li>
+                </ul>
+            </div>
+            <div class="w-[81px] h-[186px] flex flex-col align-text-top ">
+                <h3 class="text-[18px] leading-[26px] font-[700]">Company</h3>
+                <ul class="flex flex-col gap-[16px] w-full mt-[24px]">
+                    <li><a class="text-[14px] leading-[22px]">About Us</a></li>
+                    <li><a class="text-[14px] leading-[22px]">Contact Us</a></li>
+                    <li><a class="text-[14px] leading-[22px]">Our Blog</a></li>
+                    <li><a class="text-[14px] leading-[22px]">Discover</a></li>
+                </ul>
+            </div>
+
+        </div>
+
+
+            <form class="flex flex-col w-[311px] h-[106px] gap-[24px]" action="">
+                <label>Subscribe Us</label>
+                <div class="w-[311px]">
+                    <input  class="w-[311px] h-[56px] bg-transparent rounded-[10px] border border-[#343444]" placeholder="info@gmail.com" type="email">
+                    <button></button>
+
+                </div>
+
+
+            </form>
+
+    </div>
+
+</footer>
---
+++ resources/views/components/navheader.blade.php
@@ -0,0 +1,36 @@
+
+    <header class="flex w-full h-[296px] flex-col text-white">
+        <div class="flex h-[80px] items-center justify-center">
+            <div class="flex items-center justify-center gap-[10px] pr-[239px]">
+                <img src="{{asset('images/logo.svg')}}" alt="">
+                <h1 class="text-[36px] font-bold ">Axies</h1>
+            </div>
+
+            <nav class="">
+                <ul class="flex gap-[40px] items-center justify-center">
+                    <li><a class="text-[18px]" href="#">Home</a></li>
+                    <li><a class="text-[18px]" href="#">Explore</a></li>
+                    <li><a class="text-[18px]" href="">Activity</a></li>
+                    <li><a class="text-[18px]" href="#">Community</a></li>
+                    <li><a class="text-[18px]" href="#">Pages</a></li>
+                    <li><a class="text-[18px]" href="#">Contact</a></li>
+                </ul>
+            </nav>
+
+            <div class="w-[16px] h-[15px] flex items-center justify-center ml-[106px]">
+                <img src="{{asset('images/search.svg')}}" alt="">
+            </div>
+
+            <button class="w-[198px] h-[48px] border flex items-center justify-center rounded-[24px] ml-[38px] gap-[9px] border-[#5142FC]">
+                <img src="{{asset('images/wallet.svg')}}" alt="">
+                wallet connect
+            </button>
+
+        </div>
+        <div class="flex items-center justify-center">
+            <h1 class="text-[48px]">Login</h1>
+        </div>
+
+
+
+    </header>
