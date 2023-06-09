a20f14380b4ee67a9de734269f9b8156b76f980b
Onely Garay <onelygaray2002@gmail.com>
2023-06-09 09:22:38 -0600

    install laravel brezze

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
-use App\Http\Controllers\LoginController;
-use App\Http\Controllers\MainController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,13 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('layouts',[MainController::class, 'index']);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-
-// Route::get('layouts', function() {
-//     return view('layouts.login');
-// });
-
-Route::get('layouts', function() {
-    return view('layouts.signup');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
