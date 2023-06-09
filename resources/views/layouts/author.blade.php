deda02c21122fba87b827d01401fe3ce4120b98a
Onely Garay <onelygaray2002@gmail.com>
2023-06-09 15:55:30 -0600

    advanced in view author

---
+++ resources/views/layouts/author.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.main')
+
+@section('content')
+
+<div class="w-full h-[1730px]  gap-[12px] flex justify-center py-[80px] relative">
+    <div class="w-[1410px] h-[354px]">
+        <div class="h-[280px] w-full bg-[#313037] relative">
+            <div class="w-[780px] h-[274px] relative">
+                <div class="h-[274px] w-[274px] bg-[#7A798A] absolute border-[2px] border-[#5142FC] rounded-[20px]">
+
+                </div>
+                <div>
+                    
+                </div>
+            </div>
+
+        </div>
+        <div class="h-[74px] w-full bg-[#343444] relative">
+
+        </div>
+
+    </div>
+
+    {{-- aqui llevas tu logica de lo que vas a poner --}}
+
+</div>
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,8 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('layouts', function() {
+    return view('layouts.author');
+});
+
 require __DIR__.'/auth.php';
