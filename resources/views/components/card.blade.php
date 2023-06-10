5fbb54d405514fba011d1ae92bdbcfd772418817
Onely Garay <onelygaray2002@gmail.com>
2023-06-10 00:00:46 -0600

    component card is finished

---
+++ resources/views/components/card.blade.php
@@ -0,0 +1,55 @@
+<div class="bg-[#30303E] text-white rounded-[20px] px-[20px] pt-[20px] h-[511px] w-[330px]">
+    <div class="relative w-[290px] h-[290px] ">
+        <div class="flex items-center justify-center gap-[5px] bg-[#14141F] w-[64px] h-[28px] rounded-[8px] absolute z-[1] start-[214px] top-[14px]">
+            <img src="{{asset('images/heart.svg')}}" alt="">
+            <span class="text-[14px] font-[700] leading-[24px]">100</span>
+
+        </div>
+        <div class="bg-[#7A798A] w-[290px] relative h-[290px] rounded-[20px]">
+
+        </div>
+
+    </div>
+
+    <div class="w-[290px] h-[169px] mt-[21px]">
+        <h3 class="text-[18px] font-[700] leading-[26px]">Hamlet Contemplates Yorick's...</h3>
+        <div class="flex items-center mt-[17px]  w-[290px] h-[44px]">
+            <div class="flex gap-[12px] w-[146px] h-[44px]">
+                <div class="bg-[#7A798A] rounded-[15px] h-[44px] w-[44px]">
+
+                </div>
+                <div class="flex flex-col gap-[2px] h-[44px] w-[90px]">
+                    <span class="text-[#8A8AA0] text-[13px] leading-[20px]">Creator</span>
+                    <span class="text-[#EBEBEB] text-[15px] leading-[22px]">SalvadorDali</span>
+
+                </div>
+
+            </div>
+            <button class="bg-[#5142FC] text-white ml-[95px] text-[12px] font-[700] tracking-[2px] h-[24px] rounded-[8px] w-[49px]">
+                BSC
+            </button>
+
+        </div>
+
+        <hr class="w-[290px] border border-[#14141F] mt-[19px]">
+
+        <div class="h-[48px] w-[290px] flex items-center mt-[15px]">
+            <div>
+                <span class="text-[13px] leading-[20px] text-[#8A8AA0]">Current Bid</span>
+            <div class="w-[131px] h-[26px] flex items-center gap-[7px]">
+                <span class=" text-[16px] font-[700] leading-[26px]">4.89 ETH</span>
+                <span class="text-[13px] leading-[20px] text-[#8A8AA0]">=$12.246</span>
+            </div>
+
+            </div>
+
+            <div class="flex items-center gap-[8px] ml-[38px]">
+                <img src="{{asset('images/reload.svg')}}" alt="">
+                <a class="text-[16px] text-[#8A8AA0] leading-[26px]" href="#">View History</a>
+            </div>
+
+        </div>
+
+
+    </div>
+</div>
--- resources/views/layouts/author.blade.php
+++ resources/views/layouts/author.blade.php
@@ -6,7 +6,15 @@
     <div class="w-[1410px] h-[354px]">
         <div class="h-[280px] w-full bg-[#313037] relative">
             <div class="w-[780px] h-[274px] absolute mt-[40px] ml-[40px] flex gap-[40px] ">
-                <div class="h-[274px] w-[274px] bg-[#7A798A]  border-[2px] border-[#5142FC] rounded-[20px]">
+                <div class="h-[274px] w-[274px] relative isolate">
+                    <div class="h-[274px] w-[274px] bg-[#7A798A]  border-[2px] relative border-[#5142FC] rounded-[20px]">
+
+                    </div>
+                    <div class="bg-[#14141F] rounded-[8px] z-[1] absolute h-[28px] w-[64px]">
+
+
+                </div>
+
 
                 </div>
                 <div class="w-[466px] h-[186] text-white mt-[20px]">
@@ -41,58 +49,12 @@
 
     </div>
     <div class="w-[1410px] h-[1156px] mt-[60px]">
-        <div class="bg-[#30303E] text-white rounded-[20px] px-[20px] pt-[20px] h-[511px] w-[330px]">
-            <div class="bg-[#7A798A] w-[290px] h-[290px] rounded-[20px]">
-
-            </div>
-            <div class="w-[290px] h-[169px] mt-[21px]">
-                <h3 class="text-[18px] font-[700] leading-[26px]">Hamlet Contemplates Yorick's...</h3>
-                <div class="flex items-center mt-[17px]  w-[290px] h-[44px]">
-                    <div class="flex gap-[12px] w-[146px] h-[44px]">
-                        <div class="bg-[#7A798A] rounded-[15px] h-[44px] w-[44px]">
-
-                        </div>
-                        <div class="flex flex-col gap-[2px] h-[44px] w-[90px]">
-                            <span class="text-[#8A8AA0] text-[13px] leading-[20px]">Creator</span>
-                            <span class="text-[#EBEBEB] text-[15px] leading-[22px]">SalvadorDali</span>
-
-                        </div>
-
-                    </div>
-                    <button class="bg-[#5142FC] text-white ml-[95px] text-[12px] font-[700] tracking-[2px] h-[24px] rounded-[8px] w-[49px]">
-                        BSC
-                    </button>
-
-                </div>
-
-                <hr class="w-[290px] border border-[#14141F] mt-[19px]">
-
-                <div class="h-[48px] w-[290px] flex items-center">
-                    <div>
-                        <span class="text-[13px] leading-[20px] text-[#8A8AA0]">Current Bid</span>
-                    <div class="w-[131px] h-[26px] flex items-center gap-[7px]">
-                        <span class=" text-[16px] font-[700] leading-[26px]">4.89 ETH</span>
-                        <span class="text-[13px] leading-[20px] text-[#8A8AA0]">=$12.246</span>
-                    </div>
-
-                    </div>
-
-                    <div>
-                        <img src="{{asset('images/reload.svg')}}" alt="">
-                        <a href="#">View History</a>
-                    </div>
-
-                </div>
-
-
-            </div>
+        <x-card></x-card>
 
         </div>
 
     </div>
-
-    {{-- aqui llevas tu logica de lo que vas a poner --}}
-
+    
 </div>
 
 
