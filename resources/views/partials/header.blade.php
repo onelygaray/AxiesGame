6868cd700cae194856a3a94f846e4e434314d944
felixRamosLinux <fel123rodriguez@gmail.com>
2023-06-09 22:15:52 -0600

    migrations and routes

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,9 +1,11 @@
-<header class="flex w-full h-[80px] flex-col text-white">
+<header class="flex w-full h-[80px] flex-col text-white mt-5">
     <div class="flex h-[80px] items-center justify-center">
-        <div class="flex items-center justify-center gap-[10px] pr-[239px]">
+        <a class="flex items-center justify-center gap-[10px] 
+        mr-[239px] cursor-pointer hover:scale-125 ease-out duration-500
+        " href="{{ route('login')}}">
             <img src="{{asset('images/logo.svg')}}" alt="">
             <h1 class="text-[36px] font-bold ">Axies</h1>
-        </div>
+        </a>
 
         <nav class="">
             <ul class="flex gap-[40px] items-center justify-center">
@@ -13,6 +15,8 @@
                 <li><a class="text-[18px]" href="#">Community</a></li>
                 <li><a class="text-[18px]" href="#">Pages</a></li>
                 <li><a class="text-[18px]" href="#">Contact</a></li>
+                
+
             </ul>
         </nav>
 
@@ -24,6 +28,10 @@
             <img src="{{asset('images/wallet.svg')}}" alt="">
             wallet connect
         </button>
+         
+        {{-- Boton del header que redirige a la viste de crear un usuario --}}
+        <a class="text-[18px] text-white ml-[50px]
+        font-extrabold  hover:scale-125 ease-out duration-500" href="{{ route('register')}}">     Registrar   </a>
 
     </div>
 
