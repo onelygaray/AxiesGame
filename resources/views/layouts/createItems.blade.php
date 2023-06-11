c4d94a56d8dcbe7e30b0142238d2372f6b842df7
Justo Prado <pradoamoretty@gmail.com>
2023-06-10 21:29:50 -0600

    update changes v02

---
+++ resources/views/layouts/createItems.blade.php
@@ -0,0 +1,10 @@
+@include('layouts.main')
+
+@section('content')
+    <section class="h-[922px] w-[1920px] flex items-center justify-center bg-gray-800">
+        <div class="h-[922px] w-[1410px]">
+
+        </div>
+
+    </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -27,5 +27,10 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+    
+Route::get('layouts',function(){
+    return view('layouts.createItems');
+});
+
 
 require __DIR__.'/auth.php';
